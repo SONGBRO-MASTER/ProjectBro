c411471bd4dabf2eb1ef1401df5827cc429da7a7
SONGBRO <songbro@songbro.com>
2022-03-29 17:08:05 +0900

    tableView 출력
    임시 데이터 출력

--- ProjectBro/ProjectBro/ViewController.swift
+++ ProjectBro/ProjectBro/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     /* 뷰 구성 */
     private let mainTableView: UITableView = { // 메인 테이블 뷰
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
         
         return btn
     }()
+    
+    private let arr = ["1", "2", "3", "4", "5"] // 임시 데이터
 
     override func viewDidLoad() {
         
@@ -41,12 +43,31 @@ class ViewController: UIViewController {
         mainTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         mainTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         
+        mainTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell") // 테이블 뷰 셀 등록
+        
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
+        
         /* plusBtn 설정(오토레이아웃) */
         view.addSubview(plusBtn)
         plusBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         plusBtn.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -25).isActive = true
         plusBtn.widthAnchor.constraint(equalToConstant: 50).isActive = true
         plusBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = mainTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = arr[indexPath.row]
+        
+        return cell
     }
 }
 
